784edf43aedd5ac61bd00805bb4299b9b4fe8563
mohamedhassen4874.gmail.com <mohamedhassen4874.gmail.com>
2024-02-12 17:19:46 +0200

    copmeple the code of strlen

--- strlen.c
+++ strlen.c
@@ -2,6 +2,15 @@
 
 int main()
 {
-
-
+    //int length=0;
+    char *ptr;
+    char string[]="mohamed hassan ahmed";  //20
+    ptr=&string[0];
+    while(*ptr != '\0')
+    {
+       // length++;
+        ptr+=1;
+    }
+    // printf("%d\n",length);
+    printf("%d\n",(ptr-string));
 }
