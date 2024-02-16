2e257889988083fc14bf21452e7d6886e0109cc2
mohamedhassen4874.gmail.com <mohamedhassen4874.gmail.com>
2024-02-16 22:43:02 +0200

    add strcmp

---
+++ strcmp/_strcmp.h
@@ -0,0 +1,2 @@
+
+int _strcmp(char *str1,char *str2);
---
+++ strcmp/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+#include"_strcmp.h"
+
+int main()
+{
+    printf("%d\n",strcmp("mohamed","ahmed"));
+    printf("%d\n",_strcmp("mohamed","ahmed"));
+}
+
+
